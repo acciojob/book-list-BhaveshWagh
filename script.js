753c0ed9f08bd2d35d5612d85ff9acfd6abfbdc4
Bhavesh Wagh <bhaveshbm2711@gmail.com>
2024-07-01 13:31:06 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,29 @@
 //your JS code here. If required.
+// //your JS code here. If required.
+let form = document.querySelector("form");
+let bookList = document.getElementById("book-list");
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  const title = document.getElementById("title").value;
+  const author = document.getElementById("author").value;
+  const isbn = document.getElementById("isbn").value;
+
+  const inertRow = document.createElement("tr");
+  inertRow.innerHTML = `<td>${title}</td>
+                        <td>${author}</td>
+                        <td>${isbn}</td>
+    <button type="button" class="delete btn btn-danger">X</button>
+`;
+  bookList.appendChild(inertRow);
+  document.getElementById("title").value = "";
+  document.getElementById("author").value = "";
+  document.getElementById("isbn").value = "";
+
+  bookList.addEventListener('click', (e)=>{
+    if(e.target.classList.contains('delete')){
+        e.target.parentNode.parentNode.remove()
+    }
+  })
+});
